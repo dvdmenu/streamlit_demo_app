a76dcba08abcc3eaba5080b8abcf4f1534b23bdb
dvd_menu <koodaustili@gmail.com>
2023-11-13 14:37:27 +0200

    added rudimentary scatter plot for rating dependency

--- app.py
+++ app.py
@@ -6,11 +6,17 @@ df = pd.read_csv('https://raw.githubusercontent.com/dvdmenu/streamlit_demo_app/m
 
 df.head()
 
-sl.title("Title")
-sl.write("Titles are written with 'sl.title('your text')', this is written in 'sl.write()' '")
+sl.title("Insights on Rotten Tomatoes movie data from 2007 to 2011")
+sl.write("TBA: Some explanatory stuff")
 
+#this will create a scrollable window for the dataframe
 sl.write(df)
 
 
 
 sl.bar_chart(data=df, x='Audience Ratings %', y='Budget (million $)', use_container_width=True)
+
+
+
+sl.write("Relationship between critic and audience ratings:")
+sl.scatter_chart(data=df, x='Rotten Tomatoes Ratings %', y='Audience Ratings %', use_container_width=True)
