2ef14499ca26056c6bda4cdf2ebb72afcc16be1e
dvd_menu <koodaustili@gmail.com>
2023-11-14 12:43:47 +0200

    inflation correction feature

--- app.py
+++ app.py
@@ -6,17 +6,112 @@ df = pd.read_csv('https://raw.githubusercontent.com/dvdmenu/streamlit_demo_app/m
 
 df.head()
 
-sl.title("Insights on Rotten Tomatoes movie data from 2007 to 2011")
+sl.title("Insights: Rotten Tomatoes movie data from 2007 to 2011")
 sl.write("TBA: Some explanatory stuff")
 
 #this will create a scrollable window for the dataframe
 sl.write(df)
 
+sl.header("Feature engineering: Adding inflation corrected budgets to the dataframe")
+sl.write("Here is a complete procedure of adjusting movie budgets for inflation using World Bank inflation data. Be adviced that a fully working, self-updating Python CPI library exists. You should definitely take advantage of it. In this project, AFI feature was done manually for learning experience. https://pypi.org/project/cpi/")
+
+code = '''
+#  inflation rate chart downloeaded from consumer price index, filtered by USA.
+# base period 2010 = 100. This means that if 2011 CPI equals 115, prices have gone up 15 % from 2010. 
+# link https://databank.worldbank.org/reports.aspx?source=2&series=FP.CPI.TOTL&country=USA#
+
+infl = pd.read_excel('inflation_yearly.xlsx')
+
+
+# get rid of unnecessary rows and columns
+drop_these = [1,2,3,4,5]
+infl = infl.drop(index=drop_these)
+
+drop_columns = ['Series Name', 'Country Name', 'Country Code', 'Series Code']
+infl = infl.drop(columns=drop_columns)
+
+# pivot inflation table with the great Pandas melt function
+melted_infl = pd.melt(infl, var_name='Year', value_name='InflationRate')
+
+# clean unnecessary chars from year column, recode to int
+melted_infl['Year'] = melted_infl['Year'].str.extract(r'(\d{4})').astype(int)
+
+# merge dataframes, drop unnecessary double column
+df = pd.merge(df, melted_infl, left_on='Year of release', right_on='Year', how='inner')
+df.drop('Year', axis=1, inplace=True)
+
+# engineer necessary column for inflation adjusting
+# engineer inflation adjusted budget column
+
+df['inflation_factor'] = df['InflationRate'] / 100
+df['inflation_adjusted_budget'] = df['Budget (million $)'] * df['inflation_factor']
+
+#reorder columns, uniform column names
+desired_order = ['Film', 'Genre', 'Year of release', 'Rotten Tomatoes Ratings %', 'Audience Ratings %', 'Budget (million $)', 'inflation_adjusted_budget', 'InflationRate', 'inflation_factor']
+df = df[desired_order]
+
+df.rename(columns={'inflation_adjusted_budget': 'Budget (million $) AFI'}, inplace=True)
+df.rename(columns={'InflationRate': 'inflation_rate'}, inplace=True)
+
+'''
+
+
+sl.code(code, language='python', line_numbers=True)
+
+#  inflation rate chart downloeaded from consumer price index, filtered by USA.
+# base period 2010 = 100. This means that if 2011 CPI equals 115, prices have gone up 15 % from 2010. 
+# link https://databank.worldbank.org/reports.aspx?source=2&series=FP.CPI.TOTL&country=USA#
+
+infl = pd.read_excel('inflation_yearly.xlsx')
+
+
+# get rid of unnecessary rows and columns
+drop_these = [1,2,3,4,5]
+infl = infl.drop(index=drop_these)
+
+drop_columns = ['Series Name', 'Country Name', 'Country Code', 'Series Code']
+infl = infl.drop(columns=drop_columns)
+
+# pivot inflation table with the beautiful Pandas melt function
+melted_infl = pd.melt(infl, var_name='Year', value_name='InflationRate')
+
+# clean unnecessary chars from year column, recode to int
+melted_infl['Year'] = melted_infl['Year'].str.extract(r'(\d{4})').astype(int)
+
+# merge dataframes, drop unnecessary double column
+df = pd.merge(df, melted_infl, left_on='Year of release', right_on='Year', how='inner')
+df.drop('Year', axis=1, inplace=True)
+
+# engineer necessary column for inflation adjusting
+# engineer inflation adjusted budget column
+
+df['inflation_factor'] = df['InflationRate'] / 100
+df['inflation_adjusted_budget'] = df['Budget (million $)'] * df['inflation_factor']
+
+#reorder columns, uniform column names
+desired_order = ['Film', 'Genre', 'Year of release', 'Rotten Tomatoes Ratings %', 'Audience Ratings %', 'Budget (million $)', 'inflation_adjusted_budget', 'InflationRate', 'inflation_factor']
+df = df[desired_order]
+
+df.rename(columns={'inflation_adjusted_budget': 'Budget (million $) AFI'}, inplace=True)
+df.rename(columns={'InflationRate': 'inflation_rate'}, inplace=True)
+
+sl.write(df)
+
+
+
+
+# --------
+
 # bar chart on audience ratings compared to budget
-sl.bar_chart(data=df, x='Audience Ratings %', y='Budget (million $)', use_container_width=True)
+# sl.bar_chart(data=df, x='Audience Ratings %', y='Budget (million $)', use_container_width=True)
 
 
 sl.write("Relationship between critic and audience ratings:")
 
 # scatter plot that examines the rating relationship. Needs regression line and P-tests values.
 sl.scatter_chart(data=df, x='Rotten Tomatoes Ratings %', y='Audience Ratings %', use_container_width=True)
+
+
+
+
+
